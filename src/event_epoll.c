16a6ce5000cf6540cc256359fa836aed4b006f6c
Stanislav Yakush <st.yakush@yandex.ru>
2013-06-23 15:42:08 +0300

    Fixed copyright notice

--- src/event_epoll.c
+++ src/event_epoll.c
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2012, Stanislav Yakush(st.yakush@yandex.ru)
+   Copyright (c) 2009-2012, Salvatore Sanfilippo <antirez at gmail dot com>
    All rights reserved.
 
    Redistribution and use in source and binary forms, with or without
@@ -9,7 +9,7 @@
     * Redistributions in binary form must reproduce the above copyright
       notice, this list of conditions and the following disclaimer in the
       documentation and/or other materials provided with the distribution.
-    * Neither the name of the EagleMQ nor the
+    * Neither the name of Redis nor the
       names of its contributors may be used to endorse or promote products
       derived from this software without specific prior written permission.
 
@@ -24,8 +24,6 @@
    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
-
-/* Taken from Redis */
 
 #include <sys/epoll.h>
 
